7cf2496a93674becbcb66fd0d3a5236de765f1f2
fpinto <evaimitico@gmail.com>
2015-11-20 00:32:38 +0100

    second test green

--- src/org/pinto/tennisgame/Game.scala
+++ src/org/pinto/tennisgame/Game.scala
@@ -3,7 +3,22 @@
 package org.pinto.tennisgame
 
 class Game {
-   def score = "love - love"
    
-   def pointBall(player: Int) = Unit
+   var player1Score :Int = 0
+   var player2Score :Int = 0
+   
+   def score = {
+     var ret:String = "love - love"
+     if(this.player1Score > 0)
+         ret = "fifteen - love"
+     ret
+   }
+   
+   def pointBall(player: Int) = {
+     if(player == 1) 
+        this.player1Score += 1
+     else
+        this.player2Score += 1
+     Unit
+   }
 }
